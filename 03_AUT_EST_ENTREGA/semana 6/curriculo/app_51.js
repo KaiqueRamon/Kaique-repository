0d7814c0a4e762fd5c920bde304426da8c0b5552
KaiqueRamon <kaique.dantas@sou.inteli.edu.br>
2022-06-08 17:18:04 -0300

    banco

--- 03_AUT_EST_ENTREGA/semana 6/curriculo/app_51.js
+++ 03_AUT_EST_ENTREGA/semana 6/curriculo/app_51.js
@@ -8,7 +8,6 @@ app.use(express.static("."));
 app.use(express.json());
 app.get('/user1', (req, res) => {
 res.statusCode = 200;
-//res.setHeader('Content-Type', 'text/html');
 res.setHeader('Access-Control-Allow-Origin', '*');
 var db = new sqlite3.Database(DBPATH);
 var sql = 'SELECT * FROM nome';
