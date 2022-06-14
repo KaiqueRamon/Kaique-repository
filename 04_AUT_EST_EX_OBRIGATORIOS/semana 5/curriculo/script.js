7f6e2358634d00ae9a6880a528006f2f17bbd808
KaiqueRamon <kaique.dantas@sou.inteli.edu.br>
2022-06-14 14:13:16 -0300

    ex3

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/semana 5/curriculo/script.js
@@ -0,0 +1,10 @@
+ function show(){
+
+    document.getElementById("emprego1").innerHTML += "<ul> <li> Controle de almoxarifado, estoque e compras de matéria-prima <li> Cozinhador de granola (separar os ingredientes e cozinhar o produto) <li> Embalador de granola (empacotadora Golpack) <li> Atendimento ao cliente na barraca de açaí (operar o caixa e despachar a mercadoria para outras lojas) <li> Ajudante de TI no escritório (formatar PCs, instalar programas, consertar impressoras e emitir NF) </ul>"
+
+ }
+
+ function show2(){
+    document.getElementById("emprego2").innerHTML +=
+
+ }
